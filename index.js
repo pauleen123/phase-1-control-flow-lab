5786e2cf643c5d38201e9eabcc37f0f570a44ad5
+ <+>
2022-10-07 08:31:14 +0300

    commit tree 68c7dca325a95ac16635878a3cdf7a39e5e473e9

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-function scuberGreetingForFeet(feet){
-if (feet<=400)
-{
-  return 'This one is on me!';
-}
-else if (1999 < feet && feet < 2500) {
-  return "I will gladly take your thirty bucks.";
-} else {
-  return "No can do."; 
-}}
-function ternaryCheckCity (city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-}
+function scuberGreetingForFeet(ride)
 
-function switchOnCharmFromTip (tip) {
-  switch (tip) {
-    case 'generous':
-      return 'Thank you so much.';
-    case 'not as generous':
-      return 'Thank you.';
-    default:
-      return 'Bye.';
+{
+  if (ride <= 400){
+    ride = "This one is on me!"
   }
+  
+  if (ride > 2000){
+    ride = "I will gladly take your thirty bucks."
+  }
+
+  // if (ride > 2000){
+  //   ride = "I will gladly take your thirty bucks."
+  // }
+
+  return ride;
+
+  
 }
